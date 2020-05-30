fcc17eb14ccf4fdf266617283f09aa5dc0748ce8
Adrian Rios <adrian275@gmail.com>
2020-05-30 11:39:35 -0700

    Workshop Week 1 Assignment

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -25,4 +25,24 @@ campsiteRouter.route('/')
     res.end('Deleting all campsites');
 });
 
+campsiteRouter.route('/:campsiteId')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end(`Will send details of the campsite: ${req.params.campsiteId}`);
+})
+.post((req, res) => {
+    res.end(`POST operations not supported on /campsites/${req.params.campsiteId}`);
+})
+.put((req, res) => {
+    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
+    res.end(`Will update the campsites: ${req.body.name} with description: ${req.body.description}`);
+})
+.delete((req, res) => {
+    res.end(`Deleting campsite: ${req.params.campsiteId}`);
+})
+
 module.exports = campsiteRouter;
